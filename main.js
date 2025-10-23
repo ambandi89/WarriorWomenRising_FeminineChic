23b307e777786e654c766277236cbb019217fe29
ambandi89 <ambandi89@gmail.com>
2025-10-23 13:23:00 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -8,3 +8,35 @@ hamMenu.addEventListener("click", () => {
 });
 
 
+
+    // GSAP animation
+    gsap.utils.toArray(".testimonial").forEach((el, i) => {
+      gsap.to(el, {
+        scrollTrigger: {
+          trigger: el,
+          start: "top 80%",
+          toggleActions: "play none none reverse"
+        },
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: "power3.out",
+        delay: i * 0.2
+      });
+    });
+
+    // Load Lottie animations
+    const animations = [
+      { id: 'lottie-1', path: 'https://assets2.lottiefiles.com/packages/lf20_touohxv0.json' },
+      { id: 'lottie-2', path: 'https://assets2.lottiefiles.com/packages/lf20_j1adxtyb.json' }
+    ];
+
+    animations.forEach(anim => {
+      lottie.loadAnimation({
+        container: document.getElementById(anim.id),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: anim.path
+      });
+    });
